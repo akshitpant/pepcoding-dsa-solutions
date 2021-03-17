dc62ab7c859e801fdc5e8bf645f10c38bc7ae167
Akshit Pant <akpant2001@gmail.com>
2021-03-17 15:41:55 +0530

    initial commit

---
+++ Pepcoding_DS_Algo_solutions/Level 1/Basics_Of_Programming/Patterns/pattern-type-5-official.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        // input condition :   n is ODD.
+        int sp = n/2;
+        int st = 1;
+        for(int i = 1;i<=n;i++)
+        {
+          for(int j=1;j<=sp;j++)
+          {System.out.print("\t");
+              }
+          for(int j=1;j<=st;j++)
+          {System.out.print("*\t");
+              }
+        
+          if(i <=n/2)
+          {
+           sp=sp-1;
+           st=st+2;
+          }
+          
+          else
+          {
+            sp=sp+1;
+            st=st-2;
+          }
+          System.out.println();
+        }
+        
+
+    }
+}
